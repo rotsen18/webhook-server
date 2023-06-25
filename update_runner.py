1a6beba5573ed21ac83e789dc7fab07a86e94953
t.nester <t.nester@disoft.dev>
2023-06-25 16:26:55 +0300

    add build stage

--- update_runner.py
+++ update_runner.py
@@ -12,13 +12,19 @@ class Builder:
         self.application = application
 
     @staticmethod
-    def git(app_directory: str):
+    def stage_update(app_directory: str):
         app_path = os.path.join(settings.BASE_TARGET_APPS_DIR, app_directory)
         subprocess.run(['git', '-C', app_path, 'pull'])
         return True
 
     @staticmethod
-    def restart(service_name: str):
+    def stage_install_requirements(app_directory: str):
+        app_path = os.path.join(settings.BASE_TARGET_APPS_DIR, app_directory)
+        subprocess.run(['pip', 'install', '-r', os.path.join(app_path, 'requirements.txt')])
+        return True
+
+    @staticmethod
+    def stage_restart_service(service_name: str):
         return subprocess.run(['systemctl', 'restart', f'{service_name}.service'])
 
     def run(self, service_name: str = None):
@@ -26,15 +32,18 @@ class Builder:
             services_to_build = [getattr(self.application, service_name)]
         else:
             services_to_build = self.application.services
+        directory = services_to_build[0].directory
         try:
             logger.info(f'Updating {self.application} service')
-            self.git(app_directory=services_to_build[0].directory)
+            self.stage_update(app_directory=directory)
+            logger.info(f'Installing requirements for {self.application} service')
+            self.stage_install_requirements(app_directory=directory)
         except Exception as e:
             logger.debug(e)
             return False
         for service in services_to_build:
             try:
-                self.restart(service_name=service.service_name)
+                self.stage_restart_service(service_name=service.service_name)
                 logger.info(f'Restarted {service.name}.service')
             except Exception as e:
                 logger.debug(f'Failed to restart service {service.service_name}.service')
